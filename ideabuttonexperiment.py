c34139ad3ebc1a3ce76c60cab482de484e43d160
Phoebe Ireland <pireland2016@gmail.com>
2020-09-16 13:10:33 +0100

    make the thing choose a random answer

--- ideabuttonexperiment.py
+++ ideabuttonexperiment.py
@@ -1,21 +1,5 @@
-import tkinter as tk
-    
+import random
 
-def write_slogan():
-    print("Tkinter is easy to use!")
+mylist = ["apple", "banana", "cherry"]
 
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
-
-button = tk.Button(frame, 
-                   text="QUIT", 
-                   fg="red",
-                   command=quit)
-button.pack(side=tk.LEFT)
-slogan = tk.Button(frame,
-                   text="Hello",
-                   command=write_slogan)
-slogan.pack(side=tk.LEFT)
-
-root.mainloop()
+print(random.choice(mylist))

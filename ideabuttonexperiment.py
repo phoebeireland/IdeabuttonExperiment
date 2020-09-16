60bf72d2cbda3cd702f44fe379e154caaf6e747e
Phoebe Ireland <pireland2016@gmail.com>
2020-09-16 13:16:23 +0100

    combine the button with the choice maker to make the button the trigger for the random choice

--- ideabuttonexperiment.py
+++ ideabuttonexperiment.py
@@ -1,5 +1,24 @@
+import tkinter as tk
+    
 import random
 
-mylist = ["apple", "banana", "cherry"]
+mylist = ["paint", "picnic", "massage", ""]
 
-print(random.choice(mylist))
+def write_slogan():
+    print(random.choice(mylist))
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, 
+                   text="QUIT", 
+                   fg="red",
+                   command=quit)
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,
+                   text="Press me for an idea",
+                   command=write_slogan)
+slogan.pack(side=tk.LEFT)
+
+root.mainloop()

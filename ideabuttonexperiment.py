22e6d6dbc26d908db6dde1b6d96126526821706a
Phoebe Ireland <pireland2016@gmail.com>
2020-09-17 20:41:23 +0100

    put the answer in the box and make it pretty looking

--- ideabuttonexperiment.py
+++ ideabuttonexperiment.py
@@ -1,35 +1,35 @@
 import tkinter as tk
-    
+
 import random
 
-mylist = ["paint with Bob Ross", "picnic", "massage", "charcuterie board", "movie night", "blindfolded dinner"]
+mylist = [
+    "paint with Bob Ross",
+    "picnic",
+    "massage",
+    "charcuterie board",
+    "movie night",
+    "blindfolded dinner",
+]
 
-
-counter = 0 
-def counter_label(label):
-  counter = 0
-  
-
-def write_slogan():
-    print(random.choice(mylist))
 
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-root.title("Counting Seconds")
+
 label = tk.Label(root, fg="dark green")
 label.pack()
-counter_label(label)
 
-button = tk.Button(frame, 
-                   text="QUIT", 
-                   fg="red",
-                   command=quit)
-button.pack(side=tk.LEFT)
-slogan = tk.Button(frame,
-                   text="Press me for an idea",
-                   command=write_slogan)
+
+def write_slogan():
+    choice = random.choice(mylist)
+    # print(choice)
+    label.config(text=choice)
+
+
+tk.Button(frame, text="QUIT", fg="red", command=quit).pack(side=tk.LEFT)
+
+slogan = tk.Button(frame, text="Press me", command=write_slogan)
 slogan.pack(side=tk.LEFT)
 
 root.mainloop()

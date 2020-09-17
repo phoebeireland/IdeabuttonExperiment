baec89cbb10611ebd471b9305634711e94cf2b8a
Phoebe Ireland <pireland2016@gmail.com>
2020-09-17 20:06:27 +0100

    add a label space for the response

--- ideabuttonexperiment.py
+++ ideabuttonexperiment.py
@@ -4,12 +4,23 @@ import random
 
 mylist = ["paint with Bob Ross", "picnic", "massage", "charcuterie board", "movie night", "blindfolded dinner"]
 
+
+counter = 0 
+def counter_label(label):
+  counter = 0
+  
+
 def write_slogan():
     print(random.choice(mylist))
 
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
+
+root.title("Counting Seconds")
+label = tk.Label(root, fg="dark green")
+label.pack()
+counter_label(label)
 
 button = tk.Button(frame, 
                    text="QUIT", 
